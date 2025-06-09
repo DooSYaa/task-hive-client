194260bfe75f86b1eba7d40831cc5b1e53c570a4
DooSYaa <sashadubrovsky2005@gmail.com>
2025-06-09 18:00:43 +0200

    friends

---
+++ src/components/ChatComponent/InputChatComponent/InputChat.jsx
@@ -0,0 +1,12 @@
+import Button from '../../ButtonComponent/Button.jsx';
+
+export default function InputChat() {
+    return (
+        <div className="input-chat-container">
+            <input type="text"
+            className="input-chat__input"
+            placeholder="Enter message"
+            />
+        </div>
+    )
+}
